9ed5faa15d28ac1cdcff24575dba9f428269a1f5
Albin Ljunghusen <albin.ljunghusen@isotop.se>
2020-11-22 11:10:35 +0100

    Version Upgrade

--- Assets/Scripts/Coords/BorderCoord.cs
+++ Assets/Scripts/Coords/BorderCoord.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
 
 [Serializable]
@@ -36,33 +33,3 @@ public struct BorderCoord {
         return !b1.Equals(b2);
     }
 }
-
-
-[Serializable]
-public struct VertexCoord {
-    [SerializeField]
-    private Vector3Int cube;
-    [SerializeField]
-    private int index;
-
-    public VertexCoord(Vector3Int _cube, int _index) {
-        cube = _cube;
-        index = _index;
-    }
-
-
-    public Vector3Int Cube {
-        get { return cube; }
-        private set { }
-    }
-
-    public int Index {
-        get { return index; }
-        private set { }
-    }
-
-
-}
-
-
-
---
+++ Assets/Scripts/Coords/VertexCoord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct VertexCoord
+{
+    [SerializeField]
+    private Vector3Int cube;
+
+    [SerializeField]
+    private int index;
+
+    public VertexCoord(Vector3Int _cube, int _index)
+    {
+        cube = _cube;
+        index = _index;
+    }
+
+    public Vector3Int Cube
+    {
+        get { return cube; }
+        private set { }
+    }
+
+    public int Index
+    {
+        get { return index; }
+        private set { }
+    }
+}
